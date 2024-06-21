2edc0c629ec9fa819d7326c75c431150cfbeaa87
jGuru <pdfpass@outlook.com>
2024-06-21 11:54:03 +0200

    Added info to README.md

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2024.0.20")]
-[assembly: AssemblyFileVersion("2024.0.20")]
+[assembly: AssemblyVersion("2024.0.21")]
+[assembly: AssemblyFileVersion("2024.0.21")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility in Slovak")]
 [assembly: AssemblyProduct("PDFPass")]
