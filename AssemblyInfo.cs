b92798590990147223715bb7c85e3adb66f5990d
jGuru <pdfpass@outlook.com>
2024-06-21 09:49:59 +0200

    Fixed typos

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2024.0.14")]
-[assembly: AssemblyFileVersion("2024.0.14")]
+[assembly: AssemblyVersion("2024.0.20")]
+[assembly: AssemblyFileVersion("2024.0.20")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility in Slovak")]
 [assembly: AssemblyProduct("PDFPass")]
