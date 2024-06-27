2047ef8bf5374dfceeea2302103824296a73a1b5
jGuru <pdfpass@outlook.com>
2024-06-27 16:52:29 +0200

    New version release

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2024.5.1")]
-[assembly: AssemblyFileVersion("2024.5.1")]
+[assembly: AssemblyVersion("2024.5.3")]
+[assembly: AssemblyFileVersion("2024.5.3")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility in Slovak")]
 [assembly: AssemblyProduct("PDFPass")]
