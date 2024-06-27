caf66d69c0c1e49a01fadaa88cd1dee0bf13be2f
jGuru <pdfpass@outlook.com>
2024-06-27 15:54:25 +0200

    Context menu fix

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2024.5.0")]
-[assembly: AssemblyFileVersion("2024.5.0")]
+[assembly: AssemblyVersion("2024.5.1")]
+[assembly: AssemblyFileVersion("2024.5.1")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility in Slovak")]
 [assembly: AssemblyProduct("PDFPass")]
--- cmds/kontextove-menu-ako-admin-zaregistruj.cmd
+++ cmds/kontextove-menu-ako-admin-zaregistruj.cmd
@@ -10,9 +10,9 @@ if %errorlevel% == 0 (
 
 SET EXT=pdf
 SET ACTION=encrypt.2.PDF
-SET MENU_TEXT="Zahesluj PDF"
+SET MENU_TEXT="Otvor v PDFPass"
 SET EXE_FILE=%~dp0PDFPass.exe
-SET COMMAND=%EXE_FILE%\" -i \"%%1\" --owner_pass \"bbCdTWVF3$Uy6nNLsUWFAr\"
+SET COMMAND=%EXE_FILE%\" -i \"%%1\"
 
 SET REG_DIR=HKCR\SystemFileAssociations\.%EXT%\shell\%ACTION%
 REG ADD %REG_DIR% /f /ve /t REG_EXPAND_SZ /d %MENU_TEXT%
