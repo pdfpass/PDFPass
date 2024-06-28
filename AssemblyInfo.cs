cba472ca7d3422df2d2dc730f1945ba232d714e0
jGuru <pdfpass@outlook.com>
2024-06-28 11:18:13 +0200

    fixed context cmd

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2024.5.6")]
-[assembly: AssemblyFileVersion("2024.5.6")]
+[assembly: AssemblyVersion("2024.5.7")]
+[assembly: AssemblyFileVersion("2024.5.7")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility in Slovak")]
 [assembly: AssemblyProduct("PDFPass")]
--- cmds/kontextove-menu-ako-admin-zaregistruj.cmd
+++ cmds/kontextove-menu-ako-admin-zaregistruj.cmd
@@ -12,9 +12,9 @@ SET EXT=pdf
 SET ACTION=PDFPass.PDF
 SET MENU_TEXT="Otvor v PDFPass"
 SET EXE_FILE=%~dp0PDFPass.exe
-SET COMMAND=%EXE_FILE%\" -i \"%%1
+SET COMMAND=\"%EXE_FILE%\" -i \"%%1\"
 
 SET REG_DIR=HKCR\SystemFileAssociations\.%EXT%\shell\%ACTION%
 REG ADD %REG_DIR% /f /ve /t REG_EXPAND_SZ /d %MENU_TEXT%
 REG ADD "%REG_DIR%" /f /v Icon  /t REG_EXPAND_SZ /d "\"%EXE_FILE%\""
-REG ADD "%REG_DIR%\command" /f /ve /t REG_EXPAND_SZ /d "\"%COMMAND%\""
+REG ADD "%REG_DIR%\command" /f /ve /t REG_EXPAND_SZ /d "%COMMAND%"
