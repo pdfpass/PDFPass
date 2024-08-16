b3607ccd5f20b63f45bd060273c8a50284b122e5
jGuru <pdfpass@outlook.com>
2024-08-16 10:14:43 +0200

    Added InnoSetup installer to GitHub Actions

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2024.8.4")]
-[assembly: AssemblyFileVersion("2024.8.4")]
+[assembly: AssemblyVersion("2024.8.5")]
+[assembly: AssemblyFileVersion("2024.8.5")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility in Slovak")]
 [assembly: AssemblyProduct("PDFPass")]
