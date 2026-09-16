4649ae075f0ec326d678ec3505de2b07e1594989
Java Guru <jgm>
2026-09-16 19:02:55 +0200

    chore: prepare release 2026.2.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("2026.1.0")]
+[assembly: AssemblyVersion("2026.2.0")]
 [assembly: AssemblyDescription("Free PDF Encryption Utility")]
 [assembly: AssemblyProduct("PDFPass")]
 [assembly: SupportedOSPlatform("windows")]
